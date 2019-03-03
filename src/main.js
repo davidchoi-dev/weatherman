5449352409db2a169dd78057912f431365efb132
Evan Windows <bboydart91@gmail.com>
2019-03-03 23:24:52 +0900

    저장된 도시, 날씨가 있다면 API 요청 하지않도록 수정

--- src/main.js
+++ src/main.js
@@ -5,12 +5,16 @@ import Vue from 'vue';
 import App from './App';
 import router from './router';
 import store from './stores';
-import { SET_GEO_LOCATION } from 'stores/configs';
+import {
+  SET_GEO_LOCATION,
+  SET_CITY,
+  SET_WEATHER,
+  FETCH_WEATHER_BY_GEO,
+  FETCH_WEATHER_BY_CITY
+} from 'stores/configs';
+import Storage from '@/helpers/Storage';
 
 Vue.config.productionTip = false;
-
-// Find Geo
-store.dispatch(SET_GEO_LOCATION);
 
 /* eslint-disable no-new */
 new Vue({
@@ -20,3 +24,32 @@ new Vue({
   components: { App },
   template: '<App/>',
 });
+
+async function locationInitialize () {
+  // Find Geo
+  try {
+    const storedCity = Storage.getCity();
+    const storedWeather = Storage.getWeather();
+    if (storedCity) {
+      store.commit(SET_CITY, storedCity);
+    }
+
+    if (storedWeather) {
+      store.commit(SET_WEATHER, storedWeather);
+    }
+    else {
+      if (storedCity) {
+        await store.dispatch(FETCH_WEATHER_BY_CITY, storedCity);
+      }
+      else {
+        await store.dispatch(SET_GEO_LOCATION);
+        await store.dispatch(FETCH_WEATHER_BY_GEO);
+      }
+    }
+  }
+  catch (e) {
+    console.error(e);
+  }
+};
+
+locationInitialize();
