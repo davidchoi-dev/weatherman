18aeadf3931b6f9d263571ee59d3a2af3a0edf3f
Evan Moon <bboydart91@gmail.com>
2019-03-04 12:54:21 +0900

    어플리케이션 초기화 시 cities 데이터 스토어에 추가

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import router from './router';
 import store from './stores';
 import {
   SET_GEO_LOCATION,
-  SET_CITY,
-  SET_WEATHER
+  SET_CURRENT_CITY,
+  SET_WEATHER,
+  SET_CITIES
 } from 'stores/configs';
 import { STORAGE_KEYS } from '@/constants';
 import Storage from '@/helpers/Storage';
+import Cities from 'static/city.list.min';
 
 Vue.config.productionTip = false;
 
@@ -24,6 +26,8 @@ new Vue({
   template: '<App/>',
 });
 
+store.commit(SET_CITIES, Cities);
+
 function locationInitialize () {
   // Find Geo
   const storedCity = Storage.getItem(STORAGE_KEYS.CITY);
@@ -31,7 +35,7 @@ function locationInitialize () {
   const storedGeo = Storage.getItem(STORAGE_KEYS.GEO);
 
   if (storedCity) {
-    store.commit(SET_CITY, storedCity);
+    store.commit(SET_CURRENT_CITY, storedCity);
   }
   if (storedGeo) {
     store.commit(SET_GEO_LOCATION, storedGeo);
