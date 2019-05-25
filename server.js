47bf7d66c45725ac715ff415e5ecc33c9142946d
Evan Moon <bboydart91@gmail.com>
2019-05-25 14:46:01 +0900

    HTML5 히스토리 모드로 라우팅 하도록 변경

--- server.js
+++ server.js
@@ -4,9 +4,13 @@
  */
 
 const express = require('express');
-const serveStatic = require("serve-static");
-const path = require('path');
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+const fallback = require('express-history-api-fallback');
+const path = require('path');
+const distDir = path.join(__dirname, 'dist');
+
+app.use(express.static(distDir));
+app.use(fallback('index.html', { root: distDir }));
+
 const port = process.env.PORT || 80;
-app.listen(port);
+app.listen(port, () => console.log(`Serving server is listening on port ${port}`));
